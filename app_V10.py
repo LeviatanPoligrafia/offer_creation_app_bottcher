73e82cb5f1cc39ab3a467114d78028530335851c
LeviatanPoligrafia <github@leviatan.pl>
2026-02-25 09:01:33 +0100

    Refactor app_V10.py for improved structure and clarity

--- app_V10.py
+++ app_V10.py
@@ -285,7 +285,7 @@ if "generated_variants" in st.session_state and st.session_state["generated_vari
         for i, content in enumerate(st.session_state["generated_variants"], 1):
 
             #clean_content = content.lstrip('#').strip()
-            st.markdown(content)
+            st.text(content)
 
     with col2:
         # Wyświetlamy tłumaczenie tylko jeśli język docelowy był inny niż Niemiecki
@@ -302,6 +302,7 @@ if "generated_variants" in st.session_state and st.session_state["generated_vari
     if st.session_state["3_prompts"]:
 
         st.markdown(st.session_state["3_prompts"])
+
 
 
 
